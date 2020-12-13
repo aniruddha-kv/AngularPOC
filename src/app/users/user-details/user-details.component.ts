e1d0e7e60579c0b20c1e91d95bdd75f8187f1775
Aniruddha K V <aniruddhakatageri@gmail.com>
2020-12-13 23:28:33 +0530

    Small css chages

--- src/app/users/user-details/user-details.component.ts
+++ src/app/users/user-details/user-details.component.ts
@@ -9,7 +9,7 @@ import { HttpServiceService } from 'src/app/services/http-service.service';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
-  
+
   public userId;
   status: string;
   errorMessage: string;
@@ -25,51 +25,60 @@ export class UserDetailsComponent implements OnInit {
     let id = this.activatedRoute.queryParams.subscribe((val) =>
       this.userId = val.data
     );
-    this.apiUrl = this.url + '/'+ this.userId + '/posts';
+    this.apiUrl = this.url + '/' + this.userId + '/posts';
     console.log(this.apiUrl)
-    this.httpService.getUsers(this.apiUrl).subscribe(
-      (response) => {
-        this.posts = response;
-      }
-    )
+    // this.httpService.getUsers(this.apiUrl).subscribe(
+    //   (response) => {
+    //     this.posts = response;
+    //   }
+    // )
+    this.httpService.getUsers(this.apiUrl)
+      .subscribe({
+        next: (response) => {
+          this.posts = response;
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });
+
   }
 
-  deletePost(item){
-    let deleteUrl =this.mainUrl+'posts/'+item.id;
-    console.log("delete Item",item);
-    console.log("deleteURL ",deleteUrl);
+  deletePost(item) {
+    let deleteUrl = this.mainUrl + 'posts/' + item.id;
+    console.log("delete Item", item);
+    console.log("deleteURL ", deleteUrl);
     this.httpService.delete(deleteUrl)
-    .subscribe({
+      .subscribe({
         next: (data) => {
-            // console.log(data);
-            this.status = 'Delete successful';
-            // console.log(this.status);
-              let index = item.id;
-              console.log("posts Length:", this.posts.length);
-              console.log("index-1 mod 10 = ",(index-1)%10);
-              if(this.posts.length == (index-1)%10)
-              {
+          // console.log(data);
+          this.status = 'Delete successful';
+          // console.log(this.status);
+          let index = item.id;
+          console.log("posts Length:", this.posts.length);
+          console.log("index-1 mod 10 = ", (index - 1) % 10);
+          if (this.posts.length == (index - 1) % 10) {
 
-                console.log("inside if loop")
-                console.log("index",index);
-                console.log("Deleted Title ",this.posts[this.posts.length-1].title);
-                console.log("---------------------------------------")
-                this.posts.splice(-1,1);
-              }
-              else{ 
-                console.log("Else loop");
-                console.log("index",index);
-                console.log("Deleted Title ",this.posts[index%10 -1].title);
-                console.log("---------------------------------------")
-                this.posts.splice((index-1)%10, 1); 
-              }
-              console.log(this.posts);
-            
+            console.log("inside if loop")
+            console.log("index", index);
+            // console.log("Deleted Title ",this.posts[this.posts.length-1].title);
+            console.log("---------------------------------------")
+            this.posts.splice(-1, 1);
+          }
+          else {
+            console.log("Else loop");
+            console.log("index", index);
+            console.log("Deleted Title ", this.posts[index % 10 - 1].title);
+            console.log("---------------------------------------")
+            this.posts.splice((index - 1) % 10, 1);
+          }
+          console.log(this.posts);
+
         },
         error: error => {
-            this.errorMessage = error.message;
-            console.error('There was an error!', error);
+          this.errorMessage = error.message;
+          console.error('There was an error!', error);
         }
-    });
-}
+      });
+  }
 }
